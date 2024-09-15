381f76a28b22e8e0d98f721b05061a3d8e8c58d5
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-09-15 11:06:02 +0700

    Revert "Merge pull request #4 from Andrei29SE/features"
    This reverts commit 2de502a2b245f44cea828dd4ada9c01b2e11b2a4, reversing
    changes made to 3a2a8817397dcc953a1960bdff8584f1dde1a898.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import TodoActions from './componets/Todos/TodoActions';
 import TodoForm from './componets/Todos/TodoForm';
 import TodoList from './componets/Todos/TodoList';
 import { useState } from 'react';
@@ -26,23 +25,11 @@ function togleTaskHandler(id){
   }))
 
 }
-function deleteComplitedTaskHandler(){
-  setTasks(tasks.filter((task)=> !task.isComplited ))
-}
-function resetTaskHandler(){
-  setTasks([])
-}
-const taskCount = tasks.filter((task)=> task.isComplited).length
-
   return (
     <div className="App">
       <h1>App for your tasks managment</h1>
      <TodoForm addTask={addTaskHandler}/>
-     {tasks.length>0 && <TodoActions 
-     deliteComplitedTask={deleteComplitedTaskHandler} 
-     resetTask={resetTaskHandler}
-     countTask={!!taskCount}/>}
-     <TodoList count={taskCount} counter={taskCount} togletask={togleTaskHandler} deliteTask={deliteTaskHandler}
+     <TodoList togletask={togleTaskHandler} deliteTask={deliteTaskHandler}
      tasks={tasks}/>
     </div>
   );
